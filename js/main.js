11ac55e885c3c8f585f26a785bf8e89a730f6ae7
Daniel Dąbrowski <elosik123@onet.pl>
2017-01-22 20:12:51 +0100

    section skills

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 document.getElementById("hamburger").addEventListener("click", function () {
     var nav = document.getElementById("navbar");
-    
     if (nav.className === "navbar") {
         nav.className += " responsive";
     } else {
@@ -9,12 +8,29 @@ document.getElementById("hamburger").addEventListener("click", function () {
 });
 
 window.onscroll = function () {
-    
     var nav = document.getElementsByClassName("nav")[0];
-    
     if (document.body.scrollTop > 59) {
         nav.className += " sticky";
     } else if (document.body.scrollTop < 59) {
         nav.className = "nav";
     }
-}
+};
+
+$(function () {
+    var knobs = [".dial-web ", ".dial-html ", ".dial-graphic ", ".dial-ui "];
+    var colors = ["#30bae7", "#d74680", "#15c7a8", "#eb7d4b"];
+    
+    for (var i = 0; i < knobs.length; i++) {
+        $(knobs[i]).knob({
+            'readOnly': true,
+            'thickness': 0.2,
+            'font': 'Titillium Web',
+            'fgColor': colors[i],
+            'inputColor': '#3c4761',
+            'fontWeight': 100,
+            'format': function (value) {
+                return value + '%';
+            }
+        })
+    }
+});
