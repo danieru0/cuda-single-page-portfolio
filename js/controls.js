6d3a550f65f1805465822dce27e43ed7b7f47344
Daniel Dąbrowski <elosik123@onet.pl>
2017-01-25 17:44:26 +0100

    changed filterizr to mixitup

--- js/controls.js
+++ js/controls.js
@@ -1,20 +1,7 @@
 $(function() {
     //Simple filter controls
-    $('.simplefilter li').click(function() {
-        $('.simplefilter li').removeClass('active');
-        $(this).addClass('active');
-    });
-    //Multifilter controls
-    $('.multifilter li').click(function() {
-        $(this).toggleClass('active');
-    });
-    //Shuffle control
-    $('.shuffle-btn').click(function() {
-        $('.sort-btn').removeClass('active');
-    });
-    //Sort controls
-    $('.sort-btn').click(function() {
-        $('.sort-btn').removeClass('active');
+    $('.controls button').click(function() {
+        $('.controls button').removeClass('active');
         $(this).addClass('active');
     });
 });
